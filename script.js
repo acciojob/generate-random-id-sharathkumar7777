a4799df431ec6422fef379870c6be6ec95b1717c
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-24 15:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var res = "";
+	var charlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (let i = 0; i < l; i++) {
+		res += charlist.charAt(
+			Math.floor(
+				Math.random() * charlist.length
+			)
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
